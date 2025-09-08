40d684fbf63dac53849307a699c93532b9b9e809
dorname <lgqfighting@163.com>
2025-09-08 18:30:28 +0800

    更新lab2的实现

--- src/lexer.rs
+++ src/lexer.rs
@@ -108,7 +108,6 @@ impl Display for Flow {
 #[derive(Debug, PartialEq,Clone)]
 pub enum Type {
     Int,
-    Float,
     Void,
     Const
 }
@@ -117,12 +116,13 @@ impl Display for Type {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Type::Int => write!(f, "INT int"),
-            Type::Float => write!(f, "FLOAT float"),
             Type::Void => write!(f, "VOID void"),
             Type::Const => write!(f, "CONST const"),
         }
     }
 }
+
+
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Operator {
@@ -212,6 +212,7 @@ pub fn tokenizer<W:Write>(input: &str,mut w:W) ->  io::Result<()>  {
             .unwrap(); // 取第一个匹配
     let line_tokens= pair.into_inner()
         .filter_map(|p|{
+            dbg!(&p);
             if p.as_rule() == Rule::EOI {
                 None
             } else {
@@ -241,78 +242,77 @@ pub fn tokenize(input: &str)  {
 impl From<Pair<'_, Rule>> for Token {
     fn from(p: Pair<Rule>) -> Self {
         match p.as_rule() {
-            Rule::Operator => {
-                let op = p.into_inner().next().expect("Operator must have one child");
-                match op.as_rule() {
-                    Rule::Plus => Token::Operator(Operator::Plus),
-                    Rule::Minus => Token::Operator(Operator::Minus),
-                    Rule::Mul => Token::Operator(Operator::Mul),
-                    Rule::Div => Token::Operator(Operator::Div),
-                    Rule::Mod => Token::Operator(Operator::Mod),
-                    Rule::Assign => Token::Operator(Operator::Assign),
-                    Rule::Equal => Token::Operator(Operator::Equal),
-                    Rule::NotEqual => Token::Operator(Operator::NotEqual),
-                    Rule::Less => Token::Operator(Operator::Less),
-                    Rule::LessEqual => Token::Operator(Operator::LessEqual),
-                    Rule::Greater => Token::Operator(Operator::Greater),
-                    Rule::GreaterEqual => Token::Operator(Operator::GreaterEqual),
-                    Rule::And => Token::Operator(Operator::And),
-                    Rule::Or => Token::Operator(Operator::Or),
-                    Rule::Not => Token::Operator(Operator::Not),
-                    Rule::OpenParen => Token::Operator(Operator::OpenParen),
-                    Rule::CloseParen => Token::Operator(Operator::CloseParen),
-                    Rule::OpenBrace => Token::Operator(Operator::OpenBrace),
-                    Rule::CloseBrace => Token::Operator(Operator::CloseBrace),
-                    Rule::OpenBracket => Token::Operator(Operator::OpenBracket),
-                    Rule::CloseBracket => Token::Operator(Operator::CloseBracket),
-                    Rule::Comma => Token::Operator(Operator::Comma),
-                    Rule::Semicolon => Token::Operator(Operator::Semicolon),
-                    _ => panic!("expected operator,found {}", op.as_str()),
-                }
-            },
-            Rule::Flow => {
-                let op = p.into_inner().next().expect("Operator must have one child");
-                match op.as_rule() {
-                    Rule::If => Token::Flow(Flow::If),
-                    Rule::Else => Token::Flow(Flow::Else),
-                    Rule::While => Token::Flow(Flow::While),
-                    Rule::Break => Token::Flow(Flow::Break),
-                    Rule::Continue => Token::Flow(Flow::Continue),
-                    Rule::Return => Token::Flow(Flow::Return),
-                    _ => panic!("expected flow,found {}", op.as_str()),
-                }
-            },
-            Rule::Type => {
-                let op = p.into_inner().next().expect("Operator must have one child");
-                match op.as_rule() {
-                    Rule::Int => Token::Type(Type::Int),
-                    Rule::Float => Token::Type(Type::Float),
-                    Rule::Void => Token::Type(Type::Void),
-                    Rule::Const => Token::Type(Type::Const),
-                    _ => panic!("expected type,found {}", op.as_str()),
-                }
-            },
-            Rule::IntegerConst => {
-                let op = p.into_inner().next().expect("Operator must have one child");
-                match op.as_rule() {
-                    Rule::Hex => Token::IntegerConst(Hex(op.as_str().to_string())),
-                    Rule::Octal => Token::IntegerConst(Octal(op.as_str().to_string())),
-                    Rule::Dec => Token::IntegerConst(IntegerConst::Dec(op.as_str().to_string())),
-                    _ => panic!("expected integer const,found {}", op.as_str()),
-                }
-            },
-            Rule::ErrorSyntax => {
-                let op = p.into_inner().next().expect("Operator must have one child");
-                match op.as_rule() {
-                    // Rule::VarError => Token::ErrorSyntax(ErrorSyntax::VarError(op.as_str().to_string())),
-                    Rule::InvalidHex => Token::ErrorSyntax(ErrorSyntax::InvalidHex(op.as_str().to_string())),
-                    Rule::InvalidOctal => Token::ErrorSyntax(ErrorSyntax::InvalidOctal(op.as_str().to_string())),
-                    // Rule::InvalidInteger => Token::ErrorSyntax(ErrorSyntax::InvalidInteger(op.as_str().to_string())),
-                    Rule::InvalidOperator => Token::ErrorSyntax(ErrorSyntax::InvalidOperator(op.as_str().to_string())),
-                    Rule::UnKnownError => Token::ErrorSyntax(ErrorSyntax::UnKnownError(op.as_str().to_string())),
-                    _ => panic!("expected error syntax,found {}", op.as_str()),
-                }
-            },
+            // Rule::Operator => {
+            //     let op = p.into_inner().next().expect("Operator must have one child");
+            //     match op.as_rule() {
+            //         Rule::Plus => Token::Operator(Operator::Plus),
+            //         Rule::Minus => Token::Operator(Operator::Minus),
+            //         Rule::Mul => Token::Operator(Operator::Mul),
+            //         Rule::Div => Token::Operator(Operator::Div),
+            //         Rule::Mod => Token::Operator(Operator::Mod),
+            //         Rule::Assign => Token::Operator(Operator::Assign),
+            //         Rule::Equal => Token::Operator(Operator::Equal),
+            //         Rule::NotEqual => Token::Operator(Operator::NotEqual),
+            //         Rule::Less => Token::Operator(Operator::Less),
+            //         Rule::LessEqual => Token::Operator(Operator::LessEqual),
+            //         Rule::Greater => Token::Operator(Operator::Greater),
+            //         Rule::GreaterEqual => Token::Operator(Operator::GreaterEqual),
+            //         Rule::And => Token::Operator(Operator::And),
+            //         Rule::Or => Token::Operator(Operator::Or),
+            //         Rule::Not => Token::Operator(Operator::Not),
+            //         Rule::OpenParen => Token::Operator(Operator::OpenParen),
+            //         Rule::CloseParen => Token::Operator(Operator::CloseParen),
+            //         Rule::OpenBrace => Token::Operator(Operator::OpenBrace),
+            //         Rule::CloseBrace => Token::Operator(Operator::CloseBrace),
+            //         Rule::OpenBracket => Token::Operator(Operator::OpenBracket),
+            //         Rule::CloseBracket => Token::Operator(Operator::CloseBracket),
+            //         Rule::Comma => Token::Operator(Operator::Comma),
+            //         Rule::Semicolon => Token::Operator(Operator::Semicolon),
+            //         _ => panic!("expected operator,found {}", op.as_str()),
+            //     }
+            // },
+            // Rule::Flow => {
+            //     let op = p.into_inner().next().expect("Operator must have one child");
+            //     match op.as_rule() {
+            //         Rule::If => Token::Flow(Flow::If),
+            //         Rule::Else => Token::Flow(Flow::Else),
+            //         Rule::While => Token::Flow(Flow::While),
+            //         Rule::Break => Token::Flow(Flow::Break),
+            //         Rule::Continue => Token::Flow(Flow::Continue),
+            //         Rule::Return => Token::Flow(Flow::Return),
+            //         _ => panic!("expected flow,found {}", op.as_str()),
+            //     }
+            // },
+            // Rule::Type => {
+            //     let op = p.into_inner().next().expect("Operator must have one child");
+            //     match op.as_rule() {
+            //         Rule::Int => Token::Type(Type::Int),
+            //         Rule::Void => Token::Type(Type::Void),
+            //         Rule::Const => Token::Type(Type::Const),
+            //         _ => panic!("expected type,found {}", op.as_str()),
+            //     }
+            // },
+            // Rule::IntegerConst => {
+            //     let op = p.into_inner().next().expect("Operator must have one child");
+            //     match op.as_rule() {
+            //         Rule::Hex => Token::IntegerConst(Hex(op.as_str().to_string())),
+            //         Rule::Octal => Token::IntegerConst(Octal(op.as_str().to_string())),
+            //         Rule::Dec => Token::IntegerConst(IntegerConst::Dec(op.as_str().to_string())),
+            //         _ => panic!("expected integer const,found {}", op.as_str()),
+            //     }
+            // },
+            // Rule::ErrorSyntax => {
+            //     let op = p.into_inner().next().expect("Operator must have one child");
+            //     match op.as_rule() {
+            //         // Rule::VarError => Token::ErrorSyntax(ErrorSyntax::VarError(op.as_str().to_string())),
+            //         Rule::InvalidHex => Token::ErrorSyntax(ErrorSyntax::InvalidHex(op.as_str().to_string())),
+            //         Rule::InvalidOctal => Token::ErrorSyntax(ErrorSyntax::InvalidOctal(op.as_str().to_string())),
+            //         // Rule::InvalidInteger => Token::ErrorSyntax(ErrorSyntax::InvalidInteger(op.as_str().to_string())),
+            //         Rule::InvalidOperator => Token::ErrorSyntax(ErrorSyntax::InvalidOperator(op.as_str().to_string())),
+            //         Rule::UnKnownError => Token::ErrorSyntax(ErrorSyntax::UnKnownError(op.as_str().to_string())),
+            //         _ => panic!("expected error syntax,found {}", op.as_str()),
+            //     }
+            // },
             _ => {
                 Token::Identifier(p.as_str().to_string())
             }
@@ -602,5 +602,17 @@ mod tests {
         let expected_filename = FILE_PATH.to_string()+"single_ampersand_test.out";
         let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
         assert_eq!(actual, expected);
+    }
+
+
+    #[test]
+    fn test_lab2() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() +"lab2_examples/"+"test.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+        let actual = String::from_utf8(buf).unwrap();
+        println!("{:?}", actual);
     }
 }
