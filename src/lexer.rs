eb8669b6f7f2f09066de4c01b3c62b10912c6b48
dorname <lgqfighting@163.com>
2025-09-09 15:47:28 +0800

    优化词法分析规则文件

--- src/lexer.rs
+++ src/lexer.rs
@@ -193,7 +193,7 @@ pub fn tokenizer<W: Write>(input: &str, mut w: W) -> io::Result<()> {
     let line_tokens = pair
         .into_inner()
         .filter_map(|p| {
-            dbg!(&p);
+            // dbg!(&p);
             if p.as_rule() == Rule::EOI {
                 None
             } else {
@@ -270,7 +270,7 @@ impl From<Pair<'_, Rule>> for Token {
 
 #[cfg(test)]
 mod tests {
-    const FILE_PATH: &str = "tests/";
+    const FILE_PATH: &str = "tests/lab1/";
     use super::*;
 
     #[test]
@@ -281,21 +281,418 @@ mod tests {
         let file = std::fs::read_to_string(filename).expect("Failed to read file");
         let _ = tokenizer(&file, &mut buf);
 
-        let actual = String::from_utf8(buf).unwrap();
-        let expected_filename = FILE_PATH.to_string() + "arrays_and_radix.out";
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"arrays_and_radix.out";
         let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
 
         assert_eq!(actual, expected);
     }
 
     #[test]
-    fn test_lab2() {
+    fn test_comments_and_hex_files() {
         // 1、把内容输出内存缓冲区
         let mut buf = Vec::<u8>::new();
-        let filename = FILE_PATH.to_string() + "lab2_in1.txt";
+        let filename = FILE_PATH.to_string() + "comments_and_hex.in";
         let file = std::fs::read_to_string(filename).expect("Failed to read file");
         let _ = tokenizer(&file, &mut buf);
-        let actual = String::from_utf8(buf).unwrap();
-        println!("{:?}", actual);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"comments_and_hex.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+
+        assert_eq!(actual, expected);
     }
+
+    #[test]
+    fn test_complex_errors_test_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "complex_errors_test.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        actual = actual.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        let expected_filename = FILE_PATH.to_string()+"complex_errors_test.out";
+        let mut expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        expected = expected.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_complex_expressions_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "complex_expressions.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"complex_expressions.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comprehensive_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "comprehensive.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"comprehensive.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_edge_case_test_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "edge_case_test.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"edge_case_test.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_empty_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "empty.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"empty.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_error_invalid_char_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "error_invalid_char.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        actual = actual.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        let expected_filename = FILE_PATH.to_string()+"error_invalid_char.out";
+        let mut expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        expected = expected.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_invalid_character_error_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "invalid_character_error.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        actual = actual.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        let expected_filename = FILE_PATH.to_string()+"invalid_character_error.out";
+        let mut expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        expected = expected.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_keywords_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "keywords.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"keywords.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn test_leading_zeros_test_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "leading_zeros_test.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"leading_zeros_test.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_numbers_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "numbers.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"numbers.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_octal_edge_case_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "octal_edge_case.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"octal_edge_case.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_operators_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "operators.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"operators.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_sample1_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "sample1.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"sample1.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+
+    #[test]
+    fn test_sample2_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "sample2.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        actual = actual.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        let expected_filename = FILE_PATH.to_string()+"sample2.out";
+        let mut expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        expected = expected.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_sample3_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "sample3.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"sample3.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_simple_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "simple.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        let expected_filename = FILE_PATH.to_string()+"simple.out";
+        let expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        assert_eq!(actual, expected);
+    }
+
+
+    #[test]
+    fn test_single_ampersand_test_files() {
+        // 1、把内容输出内存缓冲区
+        let mut buf = Vec::<u8>::new();
+        let filename = FILE_PATH.to_string() + "single_ampersand_test.in";
+        let file = std::fs::read_to_string(filename).expect("Failed to read file");
+        let _ = tokenizer(&file, &mut buf);
+
+        let mut actual = String::from_utf8(buf).unwrap();
+        // 根据操作系统替换换行符
+        // windows下 writeln! 生成的是 \n 但从文件中读出来的换行符是 \r\n
+        actual = actual.replace("\r\n", "\n").replace('\r', "\n");
+        // 再按平台输出
+        if cfg!(windows) {
+            actual = actual.replace('\n', "\r\n");
+        }
+        actual = actual.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        let expected_filename = FILE_PATH.to_string()+"single_ampersand_test.out";
+        let mut expected = std::fs::read_to_string(expected_filename).expect("Failed to read file");
+        expected = expected.split(":").collect::<Vec<_>>().get(0).unwrap().to_string();
+        assert_eq!(actual, expected);
+    }
+
+    // #[test]
+    // fn test_lab2() {
+    //     // 1、把内容输出内存缓冲区
+    //     let mut buf = Vec::<u8>::new();
+    //     let filename = FILE_PATH.to_string() + "lab2_in1.txt";
+    //     let file = std::fs::read_to_string(filename).expect("Failed to read file");
+    //     let _ = tokenizer(&file, &mut buf);
+    //     let actual = String::from_utf8(buf).unwrap();
+    //     println!("{:?}", actual);
+    // }
 }
